89b999f244f5d5c73ae927925161da631a843caf
Melanka8 <167707648+Melanka8@users.noreply.github.com>
2024-04-21 21:16:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showRecipeInfo(id) {
+    var recipeInfo = document.getElementById(id);
+    recipeInfo.style.display = "block";
+}
+
+function hideRecipeInfo(id) {
+    var recipeInfo = document.getElementById(id);
+    recipeInfo.style.display = "none";
+}
